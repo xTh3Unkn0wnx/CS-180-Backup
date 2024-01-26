9f53a87ac4e0dc42496ed0a03b1558d12a514865
xTh3Unkn0wnx <aoceg003@ucr.edu>
2024-01-25 16:55:48 -0800

    Small changes were made

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css';
-import Navbar from './components/navbar';
-import SecondPage from './pages/SecondPage';
-import NotFoundPage from './pages/404';
+import "./App.css";
+import Navbar from "./components/navbar";
+import SecondPage from "./pages/SecondPage";
+import NotFoundPage from "./pages/404";
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios('/api/message');
+      const result = await axios("/api/message");
       setMessage(result.data.message);
     };
 
@@ -23,18 +23,21 @@ function App() {
   return (
     <Router>
       <Navbar />
-    <Routes>
-      <Route path="/" element={
-          <div>
-            <header className="App-header">
-              <p>{message}</p>
-            </header>
-          </div>
-      } />
-      <Route path="/second" Component={SecondPage} />
-      <Route path="*" Component={NotFoundPage} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <header className="App-header">
+                <p>{message}</p>
+              </header>
+            </div>
+          }
+        />
+        <Route path="/second" Component={SecondPage} />
+        <Route path="*" Component={NotFoundPage} />
+      </Routes>
+    </Router>
   );
 }
 
